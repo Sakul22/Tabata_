b2ba208836532c2bdaeb1234d04854c09ab58465
Sakul22 <lukaswendling83@gmail.com>
2026-05-17 10:09:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'tabata-v2';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
